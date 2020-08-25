b4aae777eb4c9f7524e106aadf3da014143250bb
Aziiz0 <54380763+Aziiz0@users.noreply.github.com>
2020-08-25 16:34:46 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
-#include "feature2.h"
 #include "feature1.h"
+#include "feature2.h"
 
 using namespace std;
 
 int main()
 {
   cout << "Welcome to Agile0" << endl;
-  cout << "Development Team: Adeeb, Erik, Nicholas, Anthony" << endl;feature1();
+  cout << "Development Team: Adeeb, Erik, Nicholas, and Anthony" << endl;
+  
   feature1();
   feature2();
   //feature3();
+  
   cout << endl;
   
   return 0;
