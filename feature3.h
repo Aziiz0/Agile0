d8c040e2ace03e13b82552bfa2c3798134596b37
Aziiz0 <54380763+Aziiz0@users.noreply.github.com>
2020-08-25 16:58:34 -0700

    Create feature3.h

---
+++ feature3.h
@@ -0,0 +1,12 @@
+#ifndef FEATURE3_H
+#define FEATURE3_H
+
+#include <iostream>
+
+using namespace std;
+
+void feature3(){
+    cout << "My name is Erik Martin and this is feature 3." << endl;
+}
+
+#endif FEATURE3_H
