6c8439145db60abd94d162bee6ddd926bd5657d9
SproutBoi <64399306+SproutBoi@users.noreply.github.com>
2020-08-26 01:03:48 -0700

    Update driver.cpp

--- Agile0/driver.cpp
+++ Agile0/driver.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "feature2.h"
 #include "feature3.h"
 
 using namespace std;
@@ -9,6 +10,7 @@ int main()
   cout << "Welcome to Agile0" << endl;
   cout << "Development Team: Adeeb, Erik, Nicholas, and Anthony" << endl;
   
+  feature2();
   feature3();
   
   cout << endl;
